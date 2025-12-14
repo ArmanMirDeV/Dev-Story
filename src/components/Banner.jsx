71df5833883863afed26730880111dce9d84adb6
Arman Mir <mirarman8583@gmail.com>
2025-12-14 10:13:48 +0600

    navigation with Link and Router

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,30 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Banner = () => {
+
+    const router = useRouter();
+    console.log(router);
+    
+
+  const handleBtn = () => {
+      const password = prompt("Enter Your Password!");
+      if (password == "1234") {
+          console.log(router);
+          router.push("/dashboard")
+      }
+  };
+
   return (
     <div className="text-center space-y-5 bg-linear-60  to-sky-50 from-sky-300 text-black p-10 rounded-2xl ">
-          <h2 className="text-4xl font-bold">Welcome To dev story</h2>
-          <button className="px-4 py-2 bg-sky-500 text-white rounded cursor-pointer hover:bg-black duration-200" >Share Story</button>
+      <h2 className="text-4xl font-bold">Welcome To dev story</h2>
+      <button
+        onClick={handleBtn}
+        className="px-4 py-2 bg-sky-500 text-white rounded cursor-pointer hover:bg-black duration-200"
+      >
+        Share Story
+      </button>
     </div>
   );
 };
