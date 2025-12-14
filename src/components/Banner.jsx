1d9861d9c37c942d6818bd29b7c2a74f472a3be0
Arman Mir <mirarman8583@gmail.com>
2025-12-14 10:06:18 +0600

    banner created

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="text-center space-y-5 bg-linear-60  to-sky-50 from-sky-300 text-black p-10 rounded-2xl ">
+          <h2 className="text-4xl font-bold">Welcome To dev story</h2>
+          <button className="px-4 py-2 bg-sky-500 text-white rounded cursor-pointer hover:bg-black duration-200" >Share Story</button>
+    </div>
+  );
+};
+
+export default Banner;
