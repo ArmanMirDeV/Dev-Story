aed59e9668e3a9104ed265d8843dfbfd94f4f321
Arman Mir <mirarman8583@gmail.com>
2025-12-14 09:59:15 +0600

    link skeleton added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
-    return (
-      <header className="px-4 py-3 border-b-2 border-gray-500 ">
-        <h2 className="text-lg font-semibold "> 👨‍💻 Dev Story</h2>
-      </header>
-    );
+  return (
+    <header className="px-4 py-3 border-b-2 border-gray-500 flex justify-between items-center flex-wrap ">
+      <Link href="/" className="text-lg font-semibold ">
+        {" "}
+        👨‍💻 Dev Story
+      </Link>
+
+      <nav className="space-x-2">
+        <Link href="/stories">Stories</Link>
+        <Link href="/tutorials">Tutorials</Link>
+        <Link href="/login">LogIn</Link>
+        <Link href="/register">Register</Link>
+        <Link href="/about">About</Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
